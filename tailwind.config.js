d65676bbfe3b5114f4d8d853c297b0a3b4b0bdcb
Manikandan-debugger <limacine17125@gmail.com>
2024-11-20 04:23:28 +0530

    Personal portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        herobg1: "#04016C",
+        herobg2: "#4A16BD",
+        gradient5: "#739FE8",
+        gradient6: "#4F18CA",
+        gradient7: "#D9ECFF",
+        gradient8: "#D9ECFF",
+      },
+    },
+  },
+  plugins: [],
+};
